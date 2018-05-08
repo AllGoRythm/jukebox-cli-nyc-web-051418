f155a156ba7271df2d8f1bfbd14cc4dbfcf6b164
Kurt Bauer <kurtbauer55@protonmail.com>
2018-05-08 15:07:06 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -19,17 +19,7 @@ def help
 end
 
 def list(songs)
-  if songs == true
-    puts songs = [
-      "Phoenix - 1901",
-      "Tokyo Police Club - Wait Up",
-      "Sufjan Stevens - Too Much",
-      "The Naked and the Famous - Young Blood",
-      "(Far From) Home - Tiga",
-      "The Cults - Abducted",
-      "Phoenix - Consolation Prizes",
-      "Harry Chapin - Cats in the Cradle",
-      "Amos Lee - Keep It Loose, Keep It Tight"
-    ]
+  songs.each_with_index do |variable|
+
   end
 end
