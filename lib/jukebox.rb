000d2c2dbb96d1eef8983381b315861aa8d75a18
Kurt Bauer <kurtbauer55@protonmail.com>
2018-05-08 16:48:18 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -27,7 +27,7 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   song_entered = gets.chomp
-  unless song_entered == songs.to_s || songs.to_int
+  unless song_entered == songs
     puts "Invalid input, please try again"
   else
     puts "Playing <song name>"
